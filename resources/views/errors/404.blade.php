2e9eed0d7726b4fc05d9dcbc63c7282787d73ca1
Nursaputra <nursaputra.developer@gmail.com>
2024-12-06 23:21:59 +0700

    Add view 404

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>404 Not Found</title>
+</head>
+<body>
+    <h1>404 - Halaman Tidak Ditemukan</h1>
+    <p>Halaman yang Anda cari tidak ditemukan.</p>
+    <a href="{{ url('/') }}">Kembali ke Beranda</a>
+</body>
+</html>
