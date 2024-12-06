fa30ad8016e6504e5810de5f616bcf1cf22f6131
Nursaputra <nursaputra.developer@gmail.com>
2024-12-06 23:17:37 +0700

    Update Verification Code

--- resources/views/emails/verification_code.blade.php
+++ resources/views/emails/verification_code.blade.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
 <body>
-    <h2>Your Verification Code</h2>
+    <h2>Authify | Your Verification Code</h2>
     <p>Your verification code is: <strong>{{ $code }}</strong></p>
     <p>This code will expire in 60 minutes.</p>
 </body>
