e2214ae8ff22f3e04745f30a00c844fc113ef15e
Nursaputra <nursaputra.developer@gmail.com>
2024-12-06 23:21:22 +0700

    Add view 403

---
+++ resources/views/errors/403.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>403 Forbidden</title>
+</head>
+<body>
+    <h1>403 - Forbidden</h1>
+    <p>Anda tidak memiliki akses ke halaman ini.</p>
+    <a href="{{ url('/') }}">Kembali ke Beranda</a>
+</body>
+</html>
