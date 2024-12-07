069ef45164fd66ad561bf43a951749924f288104
Nursaputra <nursaputra.developer@gmail.com>
2024-12-07 23:33:45 +0700

    Add view 402

---
+++ resources/views/errors/402.blade.php
@@ -0,0 +1,3 @@
+<h1>402 - Payment Required</h1>
+<p>Fitur ini memerlukan langganan aktif. Silakan lakukan pembayaran untuk melanjutkan.</p>
+<a href="{{ url('/subscription') }}">Langganan Sekarang</a>
